22c050e725a6be25fc726d5d85c058c9e056f1ad
cadstarxs <cardbrito315@gmail.com>
2026-04-15 21:14:12 -0300

    alterei o titulo e a descriçao do site

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
-  <title>Site TDS III</title>
+  <title>Site cardiux</title>
 </head>
 <body>
-    <?php echo "<h1>Olá, mundo!</h1>" ?>
+    <?php echo "<h1>Olá, seja bem vindo ao meu site!!!</h1>" ?>
 </body>
 </html>
